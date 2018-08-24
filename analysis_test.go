49ede603858b227faa16ffdf480703f84fbb1569
Nex <nex@nex.sx>
2018-08-25 00:47:39 +0200

    Added first unittests for analysis

---
+++ analysis_test.go
@@ -0,0 +1,60 @@
+// PhishDetect
+// Copyright (C) 2018  Claudio Guarnieri
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package phishdetect
+
+import (
+	"testing"
+)
+
+func TestBrandDetection(t *testing.T) {
+	domainsBrands := map[string]string{
+		"yahoo":     "https://www.yahoo.com",
+		"google":    "https://www.google.com",
+		"microsoft": "https://www.onedrive.com",
+		"amazon":    "http://www.amazon.co.jp",
+	}
+
+	for urlBrand, url := range domainsBrands {
+		a := NewAnalysis(url, "")
+		a.AnalyzeURL()
+		brand := a.Brands.GetBrand()
+
+		if brand != urlBrand {
+			t.Errorf("Failed to identify brand, got \"%s\" expected \"%s\"", brand, urlBrand)
+		}
+	}
+}
+
+func TestDomainWhitelist(t *testing.T) {
+	domainsWhitelist := map[string]bool{
+		"https://www.yahoo.com":      true,
+		"https://www.google.com":     true,
+		"https://www.onedrive.com":   true,
+		"http://www.amazon.co.jp":    true,
+		"http://not-real-google.com": false,
+	}
+
+	for url, expectedWhitelist := range domainsWhitelist {
+		a := NewAnalysis(url, "")
+		a.AnalyzeURL()
+
+		if a.Whitelisted != expectedWhitelist {
+			t.Errorf("Failed to identify whitelisted domain, got \"%v\" expected \"%v\"",
+				a.Whitelisted, expectedWhitelist)
+		}
+	}
+}
