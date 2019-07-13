29b278c1f4bec44a85854d70f5ebcf3866c8f557
Nex <nex@nex.sx>
2019-07-13 08:02:35 +0200

    Added custom brands support on cli

--- cli/main.go
+++ cli/main.go
@@ -19,17 +19,21 @@ package main
 import (
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/mattn/go-colorable"
 	"github.com/phishdetect/phishdetect"
+	"github.com/phishdetect/phishdetect/brand"
 	log "github.com/sirupsen/logrus"
 	flag "github.com/spf13/pflag"
+	"gopkg.in/yaml.v2"
 )
 
 var (
 	analysis *phishdetect.Analysis
 	browser  *phishdetect.Browser
+	customBrands []*brand.Brand
 )
 
 func initLogging(debug *bool) {
@@ -40,6 +44,54 @@ func initLogging(debug *bool) {
 	log.SetOutput(colorable.NewColorableStdout())
 }
 
+
+func compileBrands(brandsPath string) []*brand.Brand {
+	if brandsPath == "" {
+		return nil
+	}
+
+	if _, err := os.Stat(brandsPath); os.IsNotExist(err) {
+		log.Warning("The specified brands folder does not exist, skipping")
+		return nil
+	}
+
+	filePaths := []string{}
+	filepath.Walk(brandsPath, func(path string, info os.FileInfo, err error) error {
+		ext := filepath.Ext(strings.ToLower(path))
+		if ext == ".yaml" || ext == ".yml" {
+			filePaths = append(filePaths, path)
+		}
+		return nil
+	})
+
+	brands := []*brand.Brand{}
+
+	for _, path := range filePaths {
+		log.Debug("Trying to load custom brand file at path ", path)
+		customBrand := brand.Brand{}
+		yamlFile, err := ioutil.ReadFile(path)
+		err = yaml.Unmarshal(yamlFile, &customBrand)
+		if err != nil {
+			log.Warning("Failed to load brand file: ", err.Error())
+			continue
+		}
+
+		log.Debug("Loaded custom brand with name: ", customBrand.Name)
+
+		brands = append(brands, &customBrand)
+	}
+
+	return brands
+}
+
+func loadBrands(analysis phishdetect.Analysis) {
+	for _, customBrand := range customBrands {
+		analysis.Brands.AddBrand(customBrand)
+	}
+
+	return
+}
+
 func main() {
 	debug := flag.Bool("debug", false, "Enable debug logging")
 	tor := flag.Bool("tor", false, "Route connection through the Tor network")
@@ -48,6 +100,7 @@ func main() {
 	screenPath := flag.String("screen", "", "Specify the file path to store the screenshot")
 	safeBrowsing := flag.String("safebrowsing", "", "Specify a file path containing your Google SafeBrowsing API key")
 	container := flag.String("container", "phishdetect/phishdetect", "Specify a name for a docker image to use")
+	brandsPath := flag.String("brands", "", "Specify a folder containing YAML files with Brand specifications")
 	flag.Parse()
 	args := flag.Args()
 
@@ -59,11 +112,14 @@ func main() {
 	log.Debug("Flags: only URL analysis: ", *urlOnly)
 	log.Debug("Flags: screenshot path: ", *screenPath)
 	log.Debug("Flags: Google SafeBrowsing API key file: ", *safeBrowsing)
+	log.Debug("Flags: Brands path: ", *brandsPath)
 	log.Debug("Flags: arguments: ", args)
 
 	if len(args) == 0 {
 		log.Fatal("You need to provide a valid URL to be analyzed!")
 	}
+
+	customBrands = compileBrands(*brandsPath)
 
 	if *safeBrowsing != "" {
 		if _, err := os.Stat(*safeBrowsing); err == nil {
@@ -86,6 +142,7 @@ func main() {
 	if *urlOnly {
 		log.Debug("Instantiated url-only analysis.")
 		analysis = phishdetect.NewAnalysis(url, "")
+		loadBrands(*analysis)
 	} else {
 		browser = phishdetect.NewBrowser(phishdetect.NormalizeURL(url), *screenPath, *tor, *container)
 		err := browser.Run()
@@ -94,6 +151,7 @@ func main() {
 		}
 
 		analysis = phishdetect.NewAnalysis(url, browser.HTML)
+		loadBrands(*analysis)
 		analysis.AnalyzeHTML()
 
 		if strings.HasPrefix(browser.FinalURL, "chrome-error://") {
