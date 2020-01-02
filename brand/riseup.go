8f19ec98a065e49226706c2beaa500643b8c63d0
Nex <nex@nex.sx>
2020-01-02 16:04:55 +0100

    Updated copyright notice

--- brand/riseup.go
+++ brand/riseup.go
@@ -1,5 +1,5 @@
 // PhishDetect
-// Copyright (c) 2018-2019 Claudio Guarnieri.
+// Copyright (c) 2018-2020 Claudio Guarnieri.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
